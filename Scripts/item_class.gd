c261784c677084fe40366a7e2d0f735ff5dd88af
Margret <reff.margret@gmail.com>
2024-10-04 21:23:18 -0700

    Added Item class and a basic unfinished example

--- Scripts/item_class.gd
+++ Scripts/item_class.gd
@@ -1,2 +1,12 @@
 extends Resource
 class_name Item
+
+
+@export var name : String
+@export_multiline var description : String
+
+@export var item_texture : Texture2D
+@export var entity_texture : PackedScene
+
+@export var stackable : bool
+@export var stack_size : int
